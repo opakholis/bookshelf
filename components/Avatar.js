73bdb157e1c7f25038ad0776d96cfbb811cebdf0
Opa Kholis Majid <opakholis.id@gmail.com>
2021-06-24 13:48:30 +0700

    refactor: update sidebar

---
+++ components/Avatar.js
@@ -0,0 +1,60 @@
+export default function Avatar() {
+  return (
+    <svg
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+    >
+      <mask
+        id="mask__beam"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="36"
+        height="36"
+      >
+        <rect width="36" height="36" fill="white" rx="72"></rect>
+      </mask>
+      <g mask="url(#mask__beam)">
+        <rect width="36" height="36" fill="#f2f26f"></rect>
+        <rect
+          x="0"
+          y="0"
+          width="36"
+          height="36"
+          transform="translate(8 0) rotate(334 18 18) scale(1.1)"
+          fill="#95cfb7"
+          rx="6"
+        ></rect>
+        <g transform="translate(4 -1) rotate(-4 18 18)">
+          <path
+            d="M15 20c2 1 4 1 6 0"
+            stroke="black"
+            fill="none"
+            strokeLinecap="round"
+          ></path>
+          <rect
+            x="10"
+            y="14"
+            width="1.5"
+            height="2"
+            rx="1"
+            stroke="none"
+            fill="black"
+          ></rect>
+          <rect
+            x="24"
+            y="14"
+            width="1.5"
+            height="2"
+            rx="1"
+            stroke="none"
+            fill="black"
+          ></rect>
+        </g>
+      </g>
+    </svg>
+  );
+}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,36 @@
+import Avatar from './Avatar';
+
 export default function Sidebar() {
   return (
-    <aside className="min-w-[280px] sticky top-0 h-screen bg-blue-300">
-      <div className="flex flex-col">
-        <p>p. wip!</p>
+    <aside className="h-[93vh] sticky top-6 m-6 w-72 bg-white rounded-3xl">
+      <div className="flex flex-col px-6">
+        <div className="flex flex-col items-center my-5">
+          <Avatar />
+          <h1 className="mb-1 mt-5 text-lg font-medium">Opa Kholis Majid</h1>
+          <h3 className="w-3/4 text-center text-gray-400 text-sm italic">
+            Tidak menyukai buku fisik karena ribet!
+          </h3>
+        </div>
+        <section className="mt-10 space-y-8">
+          <div className="space-y-1">
+            <h2>Masih dipikirin</h2>
+            <div className="animate-pulse">
+              <div class="bg-pink-200 h-4 rounded" />
+            </div>
+          </div>
+          <div className="space-y-1">
+            <h2>WIP</h2>
+            <div className="animate-pulse">
+              <div class="bg-violet-200 h-4 rounded" />
+            </div>
+          </div>
+          <div className="space-y-1">
+            <h2>Lagi cari ide</h2>
+            <div className="animate-pulse">
+              <div class="bg-purple-200 h-4 rounded" />
+            </div>
+          </div>
+        </section>
       </div>
     </aside>
   );
