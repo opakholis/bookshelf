18e592ccdaaa8ab9683abc7a899377d40eb4d37d
Opa Kholis Majid <opakholis.id@gmail.com>
2021-06-25 08:07:08 +0700

    refactor: reading experience

--- components/Reading.js
+++ components/Reading.js
@@ -1,9 +1,12 @@
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Pagination } from 'swiper/core';
+import Slider from 'rc-slider';
 import TruncateMarkup from 'react-truncate-markup';
+import SwiperCore, { Autoplay } from 'swiper/core';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-SwiperCore.use([Pagination]);
+import { ArrowRight } from 'react-feather';
+
+SwiperCore.use([Autoplay]);
 
 export default function Reading({ data }) {
   console.log(data);
@@ -12,13 +15,14 @@ export default function Reading({ data }) {
       <h1 className="my-6 text-2xl font-semibold">Sedang dibaca</h1>
       <Swiper
         slidesPerView={'auto'}
-        pagination={{
-          clickable: true,
+        autoplay={{
+          delay: 5500,
+          disableOnInteraction: false,
         }}
       >
         {data.map((book, id) => (
           <SwiperSlide key={id} className="flex flex-col md:flex-row md:w-3/4">
-            <div className="relative mr-24">
+            <section className="relative mr-24">
               <div className="absolute hidden w-72 h-72 bg-blue-100 rounded-full backdrop-blur-2xl md:block" />
               <div className="w-30 h-30 md:ml-11">
                 <Image
@@ -28,27 +32,51 @@ export default function Reading({ data }) {
                   className="w-30 h-30 rounded-2xl"
                 />
               </div>
-            </div>
-            <div className="space-y-1w w-5/6 md:w-1/2 md:space-y-5">
-              <TruncateMarkup lines={2}>
-                <h2 className="text-lg font-semibold tracking-tight md:text-xl lg:text-4xl">
-                  {book?.name}
-                </h2>
-              </TruncateMarkup>
-              <h3 className="text-gray-500 text-sm uppercase md:text-base">
-                {book?.author}
-              </h3>
-              <p className="hidden pt-6 md:block">
-                {book?.genres.map((genre, id) => (
-                  <span
-                    className="mr-3 px-2 py-1 text-gray-900 text-xs bg-blue-200 rounded md:text-sm"
-                    key={id}
-                  >
-                    {genre}
-                  </span>
-                ))}
-              </p>
-            </div>
+            </section>
+            <section className="flex flex-col justify-between w-4/5 md:w-1/2">
+              <div className="space-y-1 md:space-y-2">
+                <TruncateMarkup lines={2}>
+                  <h2 className="text-lg font-semibold tracking-tight md:text-xl lg:text-4xl">
+                    {book?.name}
+                  </h2>
+                </TruncateMarkup>
+                <TruncateMarkup lines={2}>
+                  <h2 className="text-gray-500 text-sm md:text-base">
+                    {book?.summary}
+                  </h2>
+                </TruncateMarkup>
+                <h4 className="text-gray-500 text-base uppercase md:text-lg">
+                  {book?.author}
+                </h4>
+                <div>
+                  {book?.genres.map((genre, id) => (
+                    <span
+                      className="mr-1 px-2 py-1 text-sm border border-dashed border-blue-300 lowercase"
+                      key={id}
+                    >
+                      {genre}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="mb-2">
+                <div className="py-4 w-1/2">
+                  <p className="mb-1 font-medium">Progres </p>
+                  <Slider
+                    startPoint={0}
+                    max={book?.total_page}
+                    value={book?.current_page}
+                  />
+                </div>
+                {/* <p>Terakhir membaca: {book?.last_updated}</p> */}
+
+                <a href={book?.link} target="_blank" rel="noopener noreferrer">
+                  <button className="bg-blue-400/75 flex items-center justify-center py-3 w-1/3 text-white font-medium rounded-md focus:ring-2">
+                    Ayo Baca <ArrowRight className="ml-1 w-4" />
+                  </button>
+                </a>
+              </div>
+            </section>
           </SwiperSlide>
         ))}
       </Swiper>
