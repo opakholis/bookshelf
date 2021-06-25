2f8c644bb2dfffdfed7ab9770e60a83d8fd82f76
Opa Kholis Majid <opakholis.id@gmail.com>
2021-06-25 15:47:12 +0700

    style: text align to center

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,10 +4,10 @@ export default function Sidebar() {
   return (
     <aside className="h-[93.7vh] sticky top-5 hidden m-5 w-72 bg-white rounded-3xl lg:block">
       <div className="flex flex-col px-6">
-        <div className="flex flex-col items-center my-5">
+        <div className="flex flex-col items-center my-5 text-center">
           <Avatar />
           <h1 className="mb-1 mt-5 text-lg font-medium">Opa Kholis Majid</h1>
-          <h3 className="w-3/4 text-center text-gray-400 text-sm italic">
+          <h3 className="relative w-3/4 text-gray-400 text-sm italic">
             Tidak menyukai buku fisik karena ribet!
           </h3>
         </div>
