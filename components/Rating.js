c3148822dafad34d977d2c72b87d1aaaa3177bba
Opa Kholis Majid <opakholis.id@gmail.com>
2021-07-07 11:44:19 +0700

    style: replace icon

--- components/Rating.js
+++ components/Rating.js
@@ -1,4 +1,4 @@
-import { Star } from 'react-feather';
+import { Star } from 'phosphor-react';
 
 const MAX_RATING = 5;
 
@@ -8,17 +8,17 @@ export default function Rating({ rating, className }) {
       <section className="md:hidden lg:flex">
         {Array.from(Array(MAX_RATING).keys()).map((_, i) => (
           <Star
+            size={16}
+            weight={i < rating ? 'fill' : 'regular'}
             key={String(i)}
             className={
-              i < rating
-                ? 'fill-current text-yellow-300 w-5 stroke-0'
-                : 'text-gray-300 opacity-60 w-5 fill-current'
+              i < rating ? 'text-yellow-300 mr-0.5' : 'text-gray-300 mr-0'
             }
           />
         ))}
       </section>
       <section className="flex items-center lg:hidden">
-        <Star className="w-5 text-yellow-300 fill-current stroke-0" />
+        <Star size={16} weight="fill" className="w-5 text-yellow-300" />
         <span className="ml-1 font-medium">{rating}</span>/{MAX_RATING}
       </section>
     </div>
