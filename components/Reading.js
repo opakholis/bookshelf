2bd00136e81e98a51d157481568be52a934a399f
Opa Kholis Majid <opakholis.id@gmail.com>
2021-07-09 22:30:11 +0700

    refactor(reading): sort by last reading

--- components/Reading.js
+++ components/Reading.js
@@ -32,46 +32,55 @@ export default function Reading({ data }) {
           }}
           className="w-full"
         >
-          {data.map((book, id) => (
-            <SwiperSlide key={id} className="w-[300px] p-3 bg-white rounded-lg">
-              <div className="flex flex-row space-x-4">
-                <section className="relative">
-                  <div className="w-[50px] h-[75px]">
-                    <div className="rounded">
-                      <Image
-                        src={book?.cover[0].url}
-                        width={100}
-                        height={150}
-                        alt={book?.name}
-                        objectFit="cover"
-                        placeholder="blur"
-                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                          shimmer(50, 75)
-                        )}`}
-                        className="w-[50px] h-[75px] rounded"
-                      />
+          {data
+            .sort(
+              (a, b) =>
+                new Date(b.last_updated).getTime() -
+                new Date(a.last_updated).getTime()
+            )
+            .map((book, id) => (
+              <SwiperSlide
+                key={id}
+                className="w-[300px] p-3 bg-white rounded-lg"
+              >
+                <div className="flex flex-row space-x-4">
+                  <section className="relative">
+                    <div className="w-[50px] h-[75px]">
+                      <div className="rounded">
+                        <Image
+                          src={book?.cover[0].url}
+                          width={100}
+                          height={150}
+                          alt={book?.name}
+                          objectFit="cover"
+                          placeholder="blur"
+                          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                            shimmer(50, 75)
+                          )}`}
+                          className="w-[50px] h-[75px] rounded"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </section>
-                <section className="flex flex-col justify-center w-3/4 space-y-1.5">
-                  <h2 className="font-medium truncate">{book?.name}</h2>
-                  <div className="flex items-center justify-between text-gray-400 text-xs">
-                    <p className="flex items-center">
-                      <Book size={16} />
-                      <span className="ml-1">
-                        {book.current_page} / {book.total_page}
-                      </span>
-                    </p>
-                    <Persentase book={book} />
-                  </div>
-                  <ProgressBar
-                    current={book?.current_page}
-                    total={book?.total_page}
-                  />
-                </section>
-              </div>
-            </SwiperSlide>
-          ))}
+                  </section>
+                  <section className="flex flex-col justify-center w-3/4 space-y-1.5">
+                    <h2 className="font-medium truncate">{book?.name}</h2>
+                    <div className="flex items-center justify-between text-gray-400 text-xs">
+                      <p className="flex items-center">
+                        <Book size={16} />
+                        <span className="ml-1">
+                          {book.current_page} / {book.total_page}
+                        </span>
+                      </p>
+                      <Persentase book={book} />
+                    </div>
+                    <ProgressBar
+                      current={book?.current_page}
+                      total={book?.total_page}
+                    />
+                  </section>
+                </div>
+              </SwiperSlide>
+            ))}
         </Swiper>
       </section>
     </section>
