ac21a0a61448b62016a6ff260b5958daac85cc56
Opa Kholis Majid <opakholis.id@gmail.com>
2021-07-15 06:21:38 +0700

    chore: add dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'next-themes';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
