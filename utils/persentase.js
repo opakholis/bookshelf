9630fedccc8a1d57a0bf52f51018e1ccac8356dc
Opa Kholis Majid <opakholis.id@gmail.com>
2021-07-15 14:57:19 +0700

    style: more reduce code

--- utils/persentase.js
+++ utils/persentase.js
@@ -1,7 +1,5 @@
 export default function Persentase({ book }) {
-  const getValue = (currentPage, totalPage) => {
-    return (currentPage / totalPage) * 100;
-  };
+  const getValue = (currentPage, totalPage) => (currentPage / totalPage) * 100;
 
   return getValue(book?.current_page, book?.total_page).toFixed(0) + '%';
 }
