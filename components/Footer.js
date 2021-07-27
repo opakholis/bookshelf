f1dc6cf21137948fe550dead9df062c3e6d6dc99
Opa Kholis Majid <opakholis.id@gmail.com>
2021-07-27 10:54:29 +0700

    feat: add footer

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="mb-4 mt-8 px-4 w-full">
+      <p className="text-center text-gray-500 text-sm">
+        Sumber kode
+        <a
+          href="https://github.com/opxop/bookshelf"
+          className="link-custom ml-1"
+        >
+          Github
+        </a>
+      </p>
+    </footer>
+  );
+}
