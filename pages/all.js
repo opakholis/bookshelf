c409cb1e94ae731b26ff705f2985ad2d2a6f648f
Opa Kholis Majid <opakholis.id@gmail.com>
2021-07-28 13:58:45 +0700

    feat: sorting book

--- pages/all.js
+++ pages/all.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+import { Listbox } from '@headlessui/react';
+import { CaretDown, CheckCircle } from 'phosphor-react';
+
 import Container from '@/components/Container';
 import Read from '@/components/Read';
 import FormSuggestion from '@/components/FormSuggestion';
 
+const sortOptions = [{ name: 'Terbaru' }, { name: 'Rating' }];
+
 export default function All({ finished }) {
+  const [sorting, setSorting] = useState(sortOptions[0].name);
+
   return (
     <Container
       title="Koleksi Buku - Opa Kholis Majid"
@@ -27,10 +35,63 @@ export default function All({ finished }) {
         <div className="my-6">
           <FormSuggestion />
         </div>
+
+        <div className="my-6">
+          <Listbox value={sorting} onChange={setSorting}>
+            <Listbox.Label className="float-left">
+              Urutkan Berdasarkan:{' '}
+            </Listbox.Label>
+            <div className="relative w-64">
+              <Listbox.Button className="inline-flex items-center ml-1 text-groovy-blue font-medium">
+                {sorting}
+                <CaretDown size={19} weight="bold" className="pl-1" />
+              </Listbox.Button>
+              <Listbox.Options className="absolute z-50 mt-1 p-2 w-full bg-white rounded-md focus:outline-none shadow-md sm:text-sm md:left-40">
+                {sortOptions.map(({ name }) => (
+                  <Listbox.Option
+                    key={name}
+                    value={name}
+                    className={({ active }) =>
+                      `${
+                        active
+                          ? 'text-groovy-blue bg-groovy-blue/10'
+                          : 'text-gray-900'
+                      }
+                          select-none relative py-2 pl-10 pr-4 flex items-center`
+                    }
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        {name}
+                        {selected ? (
+                          <span
+                            className={`${
+                              active ? 'text-groovy-blue' : 'text-groovy-blue'
+                            } absolute inset-y-0 left-0 pl-3 flex items-center`}
+                          >
+                            <CheckCircle size={20} weight="regular" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </div>
+          </Listbox>
+        </div>
+
         <div className="grid gap-7 grid-cols-1 pt-3 md:grid-cols-3">
-          {finished.map((book) => (
-            <Read book={book} key={book.id} featured />
-          ))}
+          {finished
+            ?.sort((a, b) => {
+              if (sorting === 'Terbaru') {
+                return new Date(b.date).getTime() - new Date(a.date).getTime();
+              }
+              return b.rating - a.rating;
+            })
+            .map((book) => (
+              <Read book={book} key={book.id} featured />
+            ))}
         </div>
       </section>
     </Container>
