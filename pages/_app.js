e717147daf29f2bf70f7d1da3d16b2e2064d0d1b
Opa Kholis Majid <opakholis.id@gmail.com>
2021-08-06 17:21:22 +0700

    feat: add progressbar route

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '@/styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        color="#ac39fe"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
