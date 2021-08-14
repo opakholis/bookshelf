c9635672927f634e8e2ba771536cdc86312e4dd5
Opa Kholis Majid <opakholis.id@gmail.com>
2021-08-14 09:49:37 +0700

    refactor: improve catch error

--- pages/api/notion.js
+++ pages/api/notion.js
@@ -3,14 +3,11 @@ const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res
-      .status(405)
-      .json({ message: `${req.method} requests are not allowed` });
-  }
+  const { title } = req.body;
+
+  if (!title) return res.json(400).json({ error: 'Field is required' });
 
   try {
-    const { title } = JSON.parse(req.body);
     await notion.pages.create({
       parent: {
         database_id: process.env.NOTION_BOOK_SUGGESTIONS
@@ -28,6 +25,6 @@ export default async function handler(req, res) {
 
     res.status(201).json({ msg: 'Success' });
   } catch (error) {
-    res.status(500).json({ msg: 'There was an error' });
+    res.status(500).json({ msg: error.message || error.toString() });
   }
 }
