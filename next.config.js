9dd787ffc19f648e8873f426620dab2007b337b2
Opa Kholis Majid <opakholis.id@gmail.com>
2021-08-21 20:42:50 +0700

    switch to preact (#12)
    * chore: upgrade next v11.1.0
    
    * build: switch to preact

--- next.config.js
+++ next.config.js
@@ -2,5 +2,15 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['www.notion.so']
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preacr/test-utils',
+        'react-dom': 'preact/compat'
+      });
+    }
+    return config;
   }
 };
